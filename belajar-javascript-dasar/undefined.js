64192f4fc344129350491ee5991b16b6657765c9
Ragastra07 <haryo.accn.7@gmail.com>
2023-11-23 08:27:54 +0700

    progress for undefined  javascript basic class

---
+++ belajar-javascript-dasar/undefined.js
@@ -0,0 +1,20 @@
+let name;
+if (name === undefined){
+    alert("Undefined variable");
+} else {
+    alert("defined variable");
+}
+
+let arr = ["a", "b"];
+if (arr[2] === undefined){
+    console.info("Undefined variable");
+} else {
+    console.info("defined variable");
+}
+
+let person = {"a" : "A", "b" : "B"};
+if (person.c === undefined){
+    console.info("Undefined variable");
+} else {
+    console.info("defined variable");
+}
