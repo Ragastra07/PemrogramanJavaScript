33bd4606b163f3f0a291c717bc953d10ac894ae5
Ragastra07 <haryo.accn.7@gmail.com>
2023-12-11 15:11:51 +0700

    progress for moduleObject Javascript module

---
+++ belajar-javascript-modules/modulObject.js
@@ -0,0 +1,13 @@
+export const firstName = "tara";
+export const middleName = "arts";
+export const lastName = "indonesia";
+
+export function hello(){
+    console.info(`hello form aga`);
+}
+
+export class Person {
+    constructor(){
+        this.name = "kung";
+    }
+}
